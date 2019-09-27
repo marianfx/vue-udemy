be97e14dd56b373802b36422d6445c8e2cc1b9df
Marian Focsa <marian.focsa@kireygroup.com>
2019-09-27 12:43:21 +0300

    Added method call and update example

--- 1.getting-started/hello-world/hello-world.js
+++ 1.getting-started/hello-world/hello-world.js
@@ -2,5 +2,12 @@ new Vue({
 	el: '#app',
   data: { // available inside id='app'
   	title: "Hello World"
+  },
+  methods: {
+  	changeTitle: function(event) {
+    	this.title = event.target.value; 
+      // this will be the closest element in hierarchy => data
+      // event is the basic DOM event, with target being the input
+    }
   }
 })
