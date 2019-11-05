10a96ebce6d84bf625c9ffd9d69665964caa820a
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-05 15:24:20 +0200

    Add method instance access and v-bind example

--- 1.getting-started/hello-world/hello-world.js
+++ 1.getting-started/hello-world/hello-world.js
@@ -1,13 +1,20 @@
 new Vue({
 	el: '#app',
   data: { // available inside id='app'
-  	title: "Hello World"
+    title: "Hello, world",
+    link: "http://www.google.com"
   },
   methods: {
   	changeTitle: function(event) {
     	this.title = event.target.value; 
       // this will be the closest element in hierarchy => data
       // event is the basic DOM event, with target being the input
+    },
+    sayHello: function() {
+      // return "Hello!";
+      // return title; // this would not work
+      // return this.data.title; // this is the VUE instance => has access
+      return this.title; // vue also does a proxy so i can do this.
     }
   }
 })
