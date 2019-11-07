a6a5c8706ff3608a82054468b05e95fd7c8b96f3
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-07 18:07:30 +0200

    Add UserComponent, ready for 'props' (not added)

--- 2. cli-workflow/project-components/src/App.vue
+++ 2. cli-workflow/project-components/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <UserComponent></UserComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import UserComponent from './components/User.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    UserComponent
   }
 }
 </script>
@@ -24,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+div.component {
+    border: 1px solid black;
+    padding: 30px;
 }
 </style>
