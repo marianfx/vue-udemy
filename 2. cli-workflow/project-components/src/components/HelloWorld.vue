7e78521afd6d3ae2fa6fa00f4094d357b1578c98
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-07 17:52:04 +0200

    Add how to import components and use them, with scoped style

--- 2. cli-workflow/project-components/src/components/HelloWorld.vue
+++ 2. cli-workflow/project-components/src/components/HelloWorld.vue
@@ -3,26 +3,26 @@
 That's how Vue requires it. -->
   <div>
     <h1>Hello world!</h1>
-    <p>Server status: {{ status }}.</p>
-    <button @click="changeStatus">Change status!</button>
+    <app-server-status v-for="server in 5" :key="server"></app-server-status>
+    <!-- Another way is to not define a name and use <ServerStatus></ServerStatus> -->
   </div>
 </template>
 
 <script>
+import ServerStatus from './ServerStatus.vue';
+
 export default {
   name: "HelloWorld",
+  components: {
+    'app-server-status': ServerStatus
+  },
   // data has been created as a function and not as an object:
   // - not to mess up with references
   // - not to be missplaced with the Vue Instance 'data' property
   data: function() {
     return {
-      status: "critical"
+      servers: []
     };
-  },
-  methods: {
-    changeStatus() {
-      this.status = "normal"
-    } 
   }
 };
 </script>
---
+++ 2. cli-workflow/project-components/src/components/ServerStatus.vue
@@ -0,0 +1,35 @@
+<template>
+<!-- The template element in vue files can only have one element with children.
+That's how Vue requires it. -->
+  <div>
+    My status: {{ status }}
+    <button @click="changeStatus">Change status!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ServerStatus",
+  // data has been created as a function and not as an object:
+  // - not to mess up with references
+  // - not to be missplaced with the Vue Instance 'data' property
+  data: function() {
+    return {
+      status: "critical"
+    };
+  },
+  methods: {
+      changeStatus() {
+          this.status = "normal";
+      }
+  }
+};
+</script>
+
+<style scoped>
+ /* What I write here is global in the entire app; not highly recommended; If I add scoped => no more */
+ /* This seems like changing style does not auto refresh in browser */
+ div { 
+     border: 1px solid red;
+ }
+</style>
