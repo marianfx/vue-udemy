bbc3806793dee4b55f63850888e6f6ecfdc4a9da
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-08 12:45:05 +0200

    Add passing direct html contents from parent to child using slot

--- 2. cli-workflow/project-components-adv/src/App.vue
+++ 2. cli-workflow/project-components-adv/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <app-quote>
+      <h1>This is a quote</h1>
+      <p>This is the quote contents</p>
+    </app-quote>
   </div>
 </template>
 
 <script>
+import QuoteComponent from "./components/Quote";
 
 export default {
   name: 'app',
   components: {
+    'app-quote': QuoteComponent
   }
 }
 </script>
---
+++ 2. cli-workflow/project-components-adv/src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        theText: { type: String }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: auto;
+        text-align: center;
+    }
+</style>
