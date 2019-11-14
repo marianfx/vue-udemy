d0cdee733dfe3b68ce0817be81f3fbb5ac0cb528
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-14 17:16:07 +0200

    Computed properties are faster than filters

--- 4. addons/filters.mixins/src/App.vue
+++ 4. addons/filters.mixins/src/App.vue
@@ -4,6 +4,14 @@
     <div class="col-xs-12">
       <h1>Filters & Mixins</h1>
       <p>{{ text | toUpperCase | to-lowercase }} </p>
+      <hr>
+
+      <!-- Filters are re-rendered each time any property change.
+      Computed property change only that property changes -->
+      <input v-model="filterText" placeholder="Filter fruits">
+      <ul>
+        <li v-for="fruit in filteredFruits" :key="fruit"> {{ fruit }} </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -12,7 +20,16 @@
 export default {
   data() {
     return {
-      text: "Hello there!"
+      text: "Hello there!",
+      filterText: '',
+      fruits: ['Apple', 'Banana', 'Mango']
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      })
     }
   },
   // here they are declared with local scope. for global: Vue.filter('name', {} )
