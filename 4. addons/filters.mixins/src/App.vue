b5e4acc8a0d863738e3b86cb9d90889c1256be58
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-14 17:09:42 +0200

    Add local filter (pipe)

--- 4. addons/filters.mixins/src/App.vue
+++ 4. addons/filters.mixins/src/App.vue
@@ -1,10 +1,26 @@
 <template>
+  <!-- FILTERS ARE PIPES -->
   <div class="row">
+    <div class="col-xs-12">
+      <h1>Filters & Mixins</h1>
+      <p>{{ text | toUpperCase }} </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      text: "Hello there!"
+    }
+  },
+  // here they are declared with local scope. for global: Vue.filter('name', {} )
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase();
+    }
+  }
 };
 </script>
 
