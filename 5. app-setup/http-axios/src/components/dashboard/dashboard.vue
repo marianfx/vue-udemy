3af57254aef834d714d00ab6b1c6a4d50f82a5e0
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-15 15:53:44 +0200

    Add GET and data parsing example with AXIOS

--- 5. app-setup/http-axios/src/components/dashboard/dashboard.vue
+++ 5. app-setup/http-axios/src/components/dashboard/dashboard.vue
@@ -2,8 +2,36 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>You're email address: {{ email }} </p>
   </div>
 </template>
+
+<script>
+  import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      email: '',
+      users: []
+    };
+  },
+  created() {
+    axios.get("https://ng-vue.firebaseio.com/users.json")
+      .then((resp) => {
+        console.log(resp);
+        const data = resp.data;
+        for(let key in data) {
+          const user = data[key];
+          user.id = key;
+          this.users.push(user);
+        }
+        this.email = this.users[0].email;
+      })
+      .catch((err) => { console.log(err); });
+  }
+}
+</script>
 
 <style scoped>
   h1, p {
