c63233190bf6865b8785f6e795990efcd34cebfe
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-15 10:16:24 +0200

    Add proper implementation of filters with computed props in another component

--- 4. addons/filters.mixins/src/components/List.vue
+++ 4. addons/filters.mixins/src/components/List.vue
@@ -2,21 +2,18 @@
   <!-- FILTERS ARE PIPES -->
   <div class="row">
     <div class="col-xs-12">
-      <h1>Filters & Mixins</h1>
-      <p>{{ text | toUpperCase | to-lowercase }} </p>
-      <hr>
-
-      <app-list></app-list>
+      <input v-model="filterText" placeholder="Filter fruits">
+      <ul>
+        <li v-for="fruit in filteredFruits" :key="fruit"> {{ fruit }} </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import ListVue from './components/List.vue';
 export default {
   data() {
     return {
-      text: "Hello there!",
       filterText: '',
       fruits: ['Apple', 'Banana', 'Mango']
     }
@@ -26,15 +23,6 @@ export default {
       return this.fruits.filter((element) => {
         return element.match(this.filterText);
       })
-    }
-  },
-  components: {
-    'app-list': ListVue
-  },
-  // here they are declared with local scope. for global: Vue.filter('name', {} )
-  filters: {
-    toUpperCase(value) {
-      return value.toUpperCase();
     }
   }
 };
