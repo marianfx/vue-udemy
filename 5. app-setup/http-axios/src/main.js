eb405e03911b76410fa5f76985863b6837486567
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-15 16:03:11 +0200

    Add interceptors + eject interceptors

--- 5. app-setup/http-axios/src/main.js
+++ 5. app-setup/http-axios/src/main.js
@@ -12,6 +12,20 @@ axios.defaults.baseURL = "https://ng-vue.firebaseio.com"
 // axios.defaults.headers.common['Authorization'] = ''; // this targets all requests
 // axios.defaults.headers.get['Accepts'] = 'application/json'; // this targets only get requests
 
+// interceptors
+const reqInterceptorId = axios.interceptors.request.use(config => {
+  console.log("Intercepted", config);
+  return config; // must return, otherwise blocking
+})
+
+const respInterceptorId = axios.interceptors.response.use(response => {
+  console.log("Intercepted response", response);
+  return response;
+})
+
+// how to remove interceptors
+// axios.interceptors.request.eject(reqInterceptorId);
+// axios.interceptors.request.eject(respInterceptorId);
 
 new Vue({
   router,
