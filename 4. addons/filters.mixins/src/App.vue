c2075af873cbf03e0e99e525187f35ab2f902a4a
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-15 10:23:29 +0200

    Add bidimensional fruit list

--- 4. addons/filters.mixins/src/App.vue
+++ 4. addons/filters.mixins/src/App.vue
@@ -5,7 +5,17 @@
       <h1>Filters & Mixins</h1>
       <p>{{ text | toUpperCase | to-lowercase }} </p>
       <hr>
+      <h3> This is from App.vue</h3>
+      <div class="row">
+        <div class="col-xs-12">
+          <input v-model="filterText" placeholder="Filter fruits">
+          <ul>
+            <li v-for="fruit in filteredFruits" :key="fruit"> {{ fruit }} </li>
+          </ul>
+        </div>
+      </div>
 
+      <h3>This is from List.vue</h3>
       <app-list></app-list>
     </div>
   </div>
