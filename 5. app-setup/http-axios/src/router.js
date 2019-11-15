bc85aecd288857b5db6f76cd34391316ebcef72e
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-15 15:41:55 +0200

    Add base axios setup (has vuex and router included)

---
+++ 5. app-setup/http-axios/src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import WelcomePage from './components/welcome/welcome.vue'
+import DashboardPage from './components/dashboard/dashboard.vue'
+import SignupPage from './components/auth/signup.vue'
+import SigninPage from './components/auth/signin.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: WelcomePage },
+  { path: '/signup', component: SignupPage },
+  { path: '/signin', component: SigninPage },
+  { path: '/dashboard', component: DashboardPage },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  }
+]
+
+export default new VueRouter({mode: 'history', routes})
