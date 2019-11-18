dfefb2ec2b82ade4150986266c82a0ee3e259bf6
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 10:30:02 +0200

    Add router-link navigation (with properties)

--- 5. app-setup/router/src/App.vue
+++ 5. app-setup/router/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header></app-header>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
@@ -11,7 +12,12 @@
 </template>
 
 <script>
+    import HeaderComponent from './components/Header'
+    
     export default {
+        components: {
+            'app-header': HeaderComponent
+        }
     }
 </script>
 
---
+++ 5. app-setup/router/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <ul class="nav nav-pills">
+        <!-- tag=li => compiles it as LI -->
+        <!-- Home stays selected all time because the URL starts always with the home path. Use 'exact' to match the full path -->
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+    </ul>
+</template>
