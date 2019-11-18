fa7ac3b2d24c4fe0f59405c858255562aeaf371a
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 11:09:13 +0200

    Add sample on how to pass query params

--- 5. app-setup/router/src/components/user/UserEdit.vue
+++ 5. app-setup/router/src/components/user/UserEdit.vue
@@ -1,11 +1,14 @@
 <template>
-    <h3>Edit the User {{ id }} </h3>
+	<div>
+		<h3>Edit the User {{ id }}</h3>
+		<p>Local param: {{ $route.query.queryParam1 }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-    props: {
-        id: {}
-    }
-}
+	props: {
+		id: {}
+	}
+};
 </script>
