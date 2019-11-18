07f2b10fbf05f92c4cb0c6574070d6c50789beb4
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 10:44:44 +0200

    Add example on how to watch route and subscribe to params

--- 5. app-setup/router/src/components/Header.vue
+++ 5. app-setup/router/src/components/Header.vue
@@ -3,6 +3,8 @@
         <!-- tag=li => compiles it as LI -->
         <!-- Home stays selected all time because the URL starts always with the home path. Use 'exact' to match the full path -->
         <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-        <router-link to="/user/10" tag="li" active-class="active"><a>User</a></router-link>
+        <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+        <!-- Load same component => VUE doesn't change it if we don't react to changes (id remains 1) -->
+        <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
     </ul>
 </template>
--- 5. app-setup/router/src/components/user/User.vue
+++ 5. app-setup/router/src/components/user/User.vue
@@ -13,7 +13,12 @@ export default {
       return {
 			userId: this.$route.params.id // this exists when route is rendered, si it's ok for now
 		};
-	},
+  },
+  watch: {
+    '$route'(to, from) {
+        this.userId = to.params.id;
+    }
+  },
 	methods: {
 		navigateToHome() {
 			// router is available because of .use on Vue
