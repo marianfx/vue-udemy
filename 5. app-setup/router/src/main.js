a54b189957aa82b6e9bcda1d775e35f968932b36
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 10:16:06 +0200

    Add vue router basic setup

--- 5. app-setup/router/src/main.js
+++ 5. app-setup/router/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import { routes } from './routes';
 
 Vue.config.productionTip = false
 
+// add plugin
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes
+});
+
 new Vue({
+  router: router, // shorthand: just router
   render: h => h(App),
 }).$mount('#app')
---
+++ 5. app-setup/router/src/routes.js
@@ -0,0 +1,13 @@
+import UserComponent from './components/user/User.vue';
+import HomeComponent from './components/Home.vue';
+
+export const routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: '/user',
+        component: UserComponent
+    }
+];
