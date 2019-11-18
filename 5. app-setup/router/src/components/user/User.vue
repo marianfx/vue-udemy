ed4d3de6c68162565fcd38c76c78d82521ee30ce
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 10:34:53 +0200

    Add programatic routing sample

--- 5. app-setup/router/src/components/user/User.vue
+++ 5. app-setup/router/src/components/user/User.vue
@@ -1,4 +1,18 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr />
+    <button @click="navigateToHome">Back home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+        // router is available because of .use on Vue
+        this.$router.push('/');
+    }
+  }
+};
+</script>
