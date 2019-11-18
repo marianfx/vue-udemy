3500eb1892a6fb395eb9f5c10ae9dbc2b809e2df
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-18 11:26:50 +0200

    Add example of named routes (render different router-view-s

--- 5. app-setup/router/src/App.vue
+++ 5. app-setup/router/src/App.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="container">
-        <app-header></app-header>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
+                <router-view name="header-top"></router-view>
                 <router-view></router-view>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import HeaderComponent from './components/Header'
+    // import HeaderComponent from './components/Header'
     
     export default {
-        components: {
-            'app-header': HeaderComponent
-        }
+        // components: {
+        //     'app-header': HeaderComponent
+        // }
     }
 </script>
 
--- 5. app-setup/router/src/routes.js
+++ 5. app-setup/router/src/routes.js
@@ -3,15 +3,22 @@ import UserStartComponent from './components/user/UserStart.vue';
 import UserDetailComponent from './components/user/UserDetail.vue';
 import UserEditComponent from './components/user/UserEdit.vue';
 import HomeComponent from './components/Home.vue';
+import HeaderComponent from './components/Header.vue';
 
 export const routes = [
     {
         path: '',
-        component: HomeComponent
+        components: {
+            default: HomeComponent,
+            'header-top': HeaderComponent
+        }
     },
     {
         path: '/user',
-        component: UserComponent,
+        components: {
+            default: UserComponent,
+            'header-bottom': HeaderComponent
+        },
         children: [
             // not use slash, as it's appended on ROOT
             { path: '', component: UserStartComponent },
