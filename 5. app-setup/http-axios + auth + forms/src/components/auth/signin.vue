5c43158dfb9469ed2d29c7e156a4b6cc1d41ef8d
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-26 16:19:17 +0200

    Add signin method to firebase

--- 5. app-setup/http-axios + auth + forms/src/components/auth/signin.vue
+++ 5. app-setup/http-axios + auth + forms/src/components/auth/signin.vue
@@ -25,6 +25,7 @@
 </template>
 
 <script>
+import axios from './../../axios-auth';
   export default {
     data () {
       return {
@@ -39,6 +40,16 @@
           password: this.password,
         }
         console.log(formData)
+        axios.post("/accounts:signInWithPassword?key=AIzaSyCnLAp23j7MryCVYNC2t0vpY_GFnbymrdo", {
+          email: formData.email,
+          password: formData.password,
+          returnSecureToken: true
+        })
+          .then((r) => {
+            console.log("Finished", r);
+          }).catch((err) => {
+            console.log(err);
+          });
       }
     }
   }
