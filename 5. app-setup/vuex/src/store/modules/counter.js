4dcd41cec4593095bab5783f19b5f3837eb47e23
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-26 11:21:04 +0200

    Add example on how  to outsource to modules

---
+++ 5. app-setup/vuex/src/store/modules/counter.js
@@ -0,0 +1,46 @@
+const state = {
+    counter: 0
+};
+
+const getters = {
+    doubleCounter(state) {
+        return state.counter * 2;
+    },
+    stringCounter(state) {
+        return state.counter + " clicks";
+    },
+};
+
+const mutations = {
+    increment(state, data) {
+        state.counter += data;
+    },
+    // MUTATIONS HAVE TO BE SYNC
+    decrement(state, data) {
+        state.counter -= data;
+    },
+};
+
+const actions = {
+    // recommended to use actions to commit changes. can also use async code
+    increment(context, data) {
+        context.commit('increment', data);
+    },
+    decrement(context, data) {
+        context.commit('decrement', data);
+    },
+    incrementAsync(context, data) {
+        setTimeout( () => { context.commit('increment', data.by); }, data.time)
+        
+    },
+    decrementAsync(context, data) {
+        setTimeout( () => { context.commit('decrement', data.by); }, data.time)
+    },
+};
+
+export const counter = {
+    state,
+    getters,
+    mutations,
+    actions
+};
--- 5. app-setup/vuex/src/store/store.js
+++ 5. app-setup/vuex/src/store/store.js
@@ -1,53 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { counter } from './modules/counter';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     },
     getters: {
-        doubleCounter(state) {
-            return state.counter * 2;
-        },
-        stringCounter(state) {
-            return state.counter + " clicks";
-        },
         value(state) {
             return state.value;
         }
     },
     mutations: {
-        increment(state, data) {
-            state.counter += data;
-        },
-        // MUTATIONS HAVE TO BE SYNC
-        decrement(state, data) {
-            state.counter -= data;
-        },
         updateValue(state, data) {
             state.value = data;
         }
     },
     actions: {
-        // recommended to use actions to commit changes. can also use async code
-        increment(context, data) {
-            context.commit('increment', data);
-        },
-        decrement(context, data) {
-            context.commit('decrement', data);
-        },
-        incrementAsync(context, data) {
-            setTimeout( () => { context.commit('increment', data.by); }, data.time)
-            
-        },
-        decrementAsync(context, data) {
-            setTimeout( () => { context.commit('decrement', data.by); }, data.time)
-        },
         updateValue(context, data) {
             context.commit('updateValue', data);
         }
+    },
+    modules: {
+        counter
     }
 });
