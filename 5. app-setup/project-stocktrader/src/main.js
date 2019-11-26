b813b89b259fce7af7347888287cb1c8cbd072e7
Marian Focsa <marian.focsa@kireygroup.com>
2019-11-26 15:32:41 +0200

    Add stocks trader project

---
+++ 5. app-setup/project-stocktrader/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App.vue'
+import { routes } from './routes';
+import store from './store/store';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://ng-vue.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+new Vue({
+  store, // store: store
+  router,
+  render: h => h(App),
+}).$mount('#app')
